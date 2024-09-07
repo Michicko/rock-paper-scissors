5411fb0a1a490b2ee2bc4def691d49ee7423d8d8
Michicko <53019881+Michicko@users.noreply.github.com>
2024-09-07 15:04:53 +0100

    Added playRound feature
    - Handled all cases, draws, wins and losess
    - Handled players scores

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let humanScore = 0;
 let computerScore = 0;
 
@@ -27,4 +26,46 @@ function getHumanChoice(){
     return choice;
 }
 
-console.log(getComputerChoice(), getHumanChoice())
+function playRound(humanChoice, computerChoice){
+        if(
+            (humanChoice == 'rock' && computerChoice == 'rock') || 
+            (humanChoice === 'paper' && computerChoice == 'paper') ||
+            ( humanChoice == 'scissors' && computerChoice == 'scissors')
+        ){
+           console.log('Draw!');
+        } else if (humanChoice == 'rock'){
+            if(computerChoice == 'paper'){
+                computerScore += 1;
+               console.log("You lose! Paper beats Rock");
+            }else if(computerChoice == 'scissors'){
+                humanScore += 1;
+               console.log("You win! Rock beats Scissors");
+            }
+        } else if(humanChoice == 'paper'){
+            if(computerChoice == 'rock'){
+                humanScore += 1;
+               console.log("You win! Paper beats Rock");
+            }else if(computerChoice == 'scissors'){
+                computerScore += 1;
+               console.log("You lose! Scissors beats Paper");
+            }
+        }else if(humanChoice == 'scissors'){
+            if(computerChoice == 'rock'){
+                computerScore += 1;
+               console.log("You lose! Rock beats Scissors");
+            }else if(computerChoice == 'paper'){
+                humanScore += 1;
+               console.log("You win! Scissors beats Paper");
+            }
+        }
+
+        console.log('Scores: ', 'Player:', humanScore,':', 'Computer:', computerScore)
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+const playerScores = {
+    humanScore, computerScore
+}
+
+playRound(humanSelection, computerSelection)
