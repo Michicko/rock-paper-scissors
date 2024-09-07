0a249e84217311fad190b1fc7b528f34881f73bf
Michicko <53019881+Michicko@users.noreply.github.com>
2024-09-07 08:06:35 +0100

    created computer choice
    created a function that returns random numbers
    between 0 - 2, 0 being rock, 1 being paper and
    2 being scissors

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Rock paper scissors')
+
+function getComputerChoice(){
+    const random = Math.floor(Math.random() * 3);
+    let choice;
+    if(random == 0){
+        choice = 'rock';
+    }else if(random == 1){
+        choice = 'paper';
+    }else if (random == 2){
+        choice = 'scissors';
+    }    
+    return choice
+}
+
+getComputerChoice()
+
